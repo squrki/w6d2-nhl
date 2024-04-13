97db7e4e620440eea800e9e662a6258fa296b90e
Kirn Kim <squrkidev@gmail.com>
2024-04-12 18:44:56 -0700

    pausing 04-12-2024 1844

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+
+
+
+const teams = ["ANA", "ARI", "BOS", "BUF", "CAR", "CBJ", "CGY", "CHI", "COL", "DAL", "DET", "EDM", "FLA", "LAK", "MIN", "MTL", "NJD", "NSH", "NYI", "NYR",
+  "OTT", "PHI", "PIT", "SEA", "SJS", "STL", "TBL", "TOR", "VAN", "VGK", "WPG", "WSH"]
+
+// const [info, setInfo] = useState([])
+// https://records.nhl.com/site/api/franchise?include=teams.active&include=teams.logos
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [team, setTeam] = useState("ANA")
+  // const [info, setInfo]
+  const url = "https://api-web.nhle.com/v1/club-stats-season/"
+
+
+
+  // const getButtons = () => {
+  //   const url = "https://records.nhl.com/site/api/franchise?include=teams.active&include=teams.logos"
+  //   const data = fetch(url, { mode: 'no-cors' })
+  //   console.log(data)
+  // }
+  useEffect(() => {
+    fetch(url + team).then((response) => response.json())
+      .then((data) => {
+        console.log(data.results)
+      })
+  }, [])
+  // getButtons()
+  // const url = "https://records.nhl.com/site/api/franchise?include=teams.active&include=teams.logos"
+  // const data = fetch(url, { mode: 'no-cors' })
+  //   .then(res => res.json())
+
+  // console.log(data)
+
+
+  let element = teams.map(function (val) {
+    return (
+      <>
+        <button onClick={setTeam(val)} title={val}>{val}</button>
+      </>
+    );
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>NHL Records</h1>
+      {element}
+    </div>
   )
 }
 
 export default App
+
+
+// GET https://api-web.nhle.com/v1/club-stats-season/TEAM_ABBR
+
+// https://records.nhl.com/site/api/franchise?include=teams.id&include=teams.active&include=teams.triCode&include=teams.placeName&include=teams.commonName&include=teams.fullName&include=teams.logos&include=teams.conference.name&include=teams.division.name&include=teams.franchiseTeam.firstSeason.id&include=teams.franchiseTeam.lastSeason.id&include=teams.franchiseTeam.teamCommonName
+
+// https://records.nhl.com/site/api/franchise?include=teams.id&include=teams.active&include=teams.triCode&include=teams.logos
+
+// https://records.nhl.com/site/api/franchise?include=teams.active&include=teams.logos
